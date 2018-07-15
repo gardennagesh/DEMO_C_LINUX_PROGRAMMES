03203bd82d99cb6bcc18c6bbed33746793bf63b8
Nagesh Nanjundachari <garden.nagesh@gmail.com>
2018-07-15 19:28:12 +0530

    Update simple_shell.c

--- cpointers/simple_shell.c
+++ cpointers/simple_shell.c
@@ -2,7 +2,6 @@
 
 Programme:  basic_shell.c
 Programmer: Nagesh Nanjundachari 
-Task:       Demonstrates double pointer usage.
 Usage:      RUN as   ./a.out ls -ltr
 
 
@@ -11,7 +10,7 @@ Usage:      RUN as   ./a.out ls -ltr
 #include<unistd.h>
 
 int main (int argc, char **argv) {
-
+       
         if (!fork()) {
                 argv++;
                 execvp(argv[0], argv);
